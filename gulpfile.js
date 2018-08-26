b180578b3d367a3664c6c82f2d582879ff6f4540
Steven Enamakel <enamakel@cryptocontrol.io>
2018-08-26 13:38:16 +0530

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var exec = require('child_process').exec
+
+var gulp = require('gulp')
+var gulpsync = require('gulp-sync')(gulp)
+var ghPages = require('gulp-gh-pages')
+var surge = require('gulp-surge')
+var rename = require('gulp-rename');
+
+// meteor-build-client ../build
+gulp.task('build', function (cb) {
+  exec('meteor-build-client ../dist --path ""', {cwd: 'frontend'}, function (err, res, failed) {
+    if (err) {
+      console.log(err)
+    } else if (failed) {
+      process.stdout.write(failed)
+    } else {
+      process.stdout.write('\u001b[32mMeteor build completed!\n')
+    }
+    cb(err)
+  })
+})
+
+// gh-pages
+gulp.task('deploy-gh-pages', function () {
+  require('fs').writeFileSync('./dist/CNAME', 'dex.maharajadao.com');
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages())
+})
+
+gulp.task('deploy-surge', [], function () {
+  return surge({
+    project: './dist',          // Path to your static build directory
+    domain: 'https://dex.maharajadao.com'  // Your domain or Surge subdomain
+  })
+})
+
+gulp.task('deploy', gulpsync.sync(['build', 'deploy-gh-pages']))
